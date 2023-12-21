ebdea241e8f5e74e8fac534d05596beee25be9ea
SilviMarku <marku.silvia777@gmail.com>
2023-12-21 15:52:32 +0100

    A

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from api.v1.views.index import *
+from api.v1.views.states import *
+from flask import Blueprint
+"""
+Moudle to create Blueprint for our API
+"""
+
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
